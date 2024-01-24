e767242351d5be4878b8fa1e5cec56238bf505b6
ASUS <ASUS@KUSHANI>
2024-01-24 14:59:03 +0530

    Add project files.

---
+++ Tutorial 01- 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_01__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum =0;
+
+            Console.Write("Enter a Positive number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > 0) 
+            {
+                for(int i=1; i <= number; i++) 
+                {
+                    sum = sum + i;
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
